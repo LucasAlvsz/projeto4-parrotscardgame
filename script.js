6439b3252a694ecb849e26196d82589d02abc6ca
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-02-04 14:47:00 -0300

    Implementado aleatoriedade das cartas e permanecer virada ou desvirarem após 1s

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
-function virarCarta(botao) {
-    let idFrente = botao.querySelector(".face")
-    let idVerso = botao.querySelector(".verso")
-    idFrente.classList.toggle("frente-rotate")
-    idVerso.classList.toggle("verso-rotate")
+let comparaId = []
+let armazenaCarta = []
+function virarCarta(cartaClicada) {
+    let classFrente = cartaClicada.querySelector(".face")
+    let classVerso = cartaClicada.querySelector(".verso")
+    classFrente.classList.toggle("frente-rotate")
+    classVerso.classList.toggle("verso-rotate")
+    comparaId.push(cartaClicada.id)
+    armazenaCarta.push(cartaClicada)
+    // Caso as cartas sejam iguais, permanencem viradas.
+    if (comparaId.length == 2){
+        if (comparaId[0] == comparaId[1]) {
+            armazenaCarta[0].disabled = true
+            armazenaCarta[1].disabled = true
+            comparaId = []
+            armazenaCarta = []
+        } else{
+            // Desvira as cartas após 1 segundo, caso elas sejam diferentes.
+            setTimeout(() => {
+                armazenaCarta[0].querySelector(".face").classList.toggle("frente-rotate")
+                armazenaCarta[0].querySelector(".verso").classList.toggle("verso-rotate")
+                classFrente.classList.toggle("frente-rotate")
+                classVerso.classList.toggle("verso-rotate")
+                armazenaCarta = []
+            }, 1000);
+            comparaId = []
+        }
+    }
+
 }
 
-
 let cartas = []
-const card = `
-<div class="card" onclick="virarCarta(this)">
-            <div class="face"></div>
-            <div class="face verso"></div>
-</div>
- `
 let numeroDeCartas = parseInt(prompt("Com quantas cartas você deseja jogar"))
 while (numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 != 0) {
     numeroDeCartas = parseInt(prompt("Com quantas cartas você deseja jogar"))
 }
-console.log(numeroDeCartas)
 const main = document.querySelector("main")
-console.log(main)
-
-for (let i = 0; i < numeroDeCartas; i++) {
+for (let i = 0; i < numeroDeCartas / 2; i++) {
+    let card = `
+    <button class="card" id="${i}" onclick="virarCarta(this)">
+        <div class="face">
+            <img src="gifs/front.png" alt="front">
+        </div>
+        <div class="face verso">
+               <img src="gifs/gif${i}.gif" alt="gif${i}">
+        </div>
+    </button>
+    `
     cartas.push(card)
-
+    cartas.push(card)
 }
+// 
+function comparador() {
+    return Math.random() - 0.5;
+}
+cartas.sort(comparador)
 main.innerHTML = cartas.toString().replace(/,/g, "")
+
